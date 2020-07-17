f30232edc651d8a0b2955058ee339d1332cd67a7
cremoon <bartels.maik@gmail.com>
2020-07-17 11:47:25 +0200

    package correction

--- src/test/java/guru/springframework/sfgdi/SfgDiApplicationTests.java
+++ src/test/java/guru/springframework/sfgdi/SfgDiApplicationTests.java
@@ -1,10 +1,10 @@
-package guru.springframework.sagdi;
+package guru.springframework.sfgdi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SagDiApplicationTests {
+class SfgDiApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.ConstructorInjectedGreetingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by maikbartels on 2020.07.16
+ */
+class SetterInjectedControllerTest {
+    SetterInjectedController setterInjectedController;
+
+    @BeforeEach
+    void setUp() {
+        setterInjectedController = new SetterInjectedController();
+        setterInjectedController.setGreetingService(new ConstructorInjectedGreetingService());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(setterInjectedController.getGreeting());
+    }
+}
